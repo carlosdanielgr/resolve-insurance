bddf29fb179f3424f24d024d59e7bba4f6701529
carlosdanielgr <carlos.dgr.12@gmail.com>
2024-03-23 18:10:58 -0500

    fourth content completed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,6 +4,7 @@ import { HeaderComponent } from './components/header/header.component';
 import { FirstContentComponent } from './components/first-content/first-content.component';
 import { SecondContentComponent } from './components/second-content/second-content.component';
 import { ThirdContentComponent } from './components/third-content/third-content.component';
+import { FourthContentComponent } from './components/fourth-content/fourth-content.component';
 
 @Component({
   selector: 'app-root',
@@ -13,6 +14,7 @@ import { ThirdContentComponent } from './components/third-content/third-content.
     FirstContentComponent,
     SecondContentComponent,
     ThirdContentComponent,
+    FourthContentComponent,
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
---
+++ src/app/components/fourth-content/fourth-content.component.ts
@@ -0,0 +1,45 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-fourth-content',
+  standalone: true,
+  imports: [],
+  templateUrl: './fourth-content.component.html',
+  styleUrl: './fourth-content.component.scss',
+})
+export class FourthContentComponent {
+  data = [
+    {
+      icon: './assets/icons/ball.svg',
+      text: 'Accidentes escolares y deportivos',
+    },
+    {
+      icon: './assets/icons/ubication.svg',
+      text: 'Seguros de viaje',
+    },
+    {
+      icon: './assets/icons/money.svg',
+      text: 'Ahorro para el retiro',
+    },
+    {
+      icon: './assets/icons/medal.svg',
+      text: 'Planes educacionales',
+    },
+    {
+      icon: './assets/icons/house-i.svg',
+      text: 'Seguro casa habitación',
+    },
+    {
+      icon: './assets/icons/dollar.svg',
+      text: 'Plan de vida financiera',
+    },
+    {
+      icon: './assets/icons/heart.svg',
+      text: 'Planes de Salud',
+    },
+    {
+      icon: './assets/icons/biking.svg',
+      text: 'Motos y bicicletas',
+    },
+  ];
+}
